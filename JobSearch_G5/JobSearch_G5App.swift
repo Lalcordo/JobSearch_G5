c3cbf39aa18e3bfd2f2b02fe4e8178de4bad9ab6
Leo <leocesar_alcordo@hotmail.com>
2024-03-09 15:04:22 -0500

    Initial Commit

---
+++ JobSearch_G5/JobSearch_G5App.swift
@@ -0,0 +1,17 @@
+//
+//  JobSearch_G5App.swift
+//  JobSearch_G5
+//
+//  Created by Leo Cesar Alcordo on 2024-03-09.
+//
+
+import SwiftUI
+
+@main
+struct JobSearch_G5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
